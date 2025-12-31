080ccfd230c550c035a0afff1d95bda730885e90
Snehal1808 <snehalsubu18@gmail.com>
2025-12-31 18:32:39 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,11 @@
-CREATE TABLE weather_logs (
+CREATE DATABASE weather_db;
+USE weather_db;
+
+CREATE TABLE daily_weather (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    city VARCHAR(50),
+    city VARCHAR(100),
     temperature FLOAT,
     humidity INT,
-    pressure INT,
-    description VARCHAR(100),
-    recorded_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    description VARCHAR(255),
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
